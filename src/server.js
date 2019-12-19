aebca14485f41138322552312c0d49686b8771a4
canikwe <c.anikwe@gmail.com>
2019-12-18 20:54:47 -0500

    add and configure yelp api and endpoint

--- src/server.js
+++ src/server.js
@@ -2,7 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const fetch = require('node-fetch')
+const bluebird = require('bluebird')
+fetch.promise = bluebird
+
+// Database queries
 const db = require('../queries.js')
+
+// Configure dotenv for environment variables
+const dotenv = require('dotenv')
+dotenv.config()
 
 // Apply express json parser to parse body from POST requests
 app.use(express.json())
@@ -13,6 +22,25 @@ app.get('/', (req, res) => {
   })
 })
 
+// Yelp calls
+app.get('/yelp/:term/:location', (req, res) => {
+  fetch(`https://api.yelp.com/v3/businesses/search?term=${req.params.term}&location=${req.params.location}&limit=10`, {
+    headers: {
+      Authorization: `Bearer ${process.env.api_key}`
+    }
+  })
+  .then(resp => resp.json())
+  .then(data => {
+    console.log(data.businesses)
+    res.send(data.businesses)
+  })
+  .catch(err => {
+    console.log(err)
+    return res.send(err)
+  })
+})
+
+// Database queries
 app.get('/api/users', db.getAllUsers)
 app.get('/api/users/:id', db.getSingleUser)
 app.post('/api/users', db.createUser)
